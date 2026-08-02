d3001343387dd13286277307de231a2cf7c676cd
Adriel Samuel Tettey <sapntettey@st.ug.edu.gh>
2026-08-02 19:44:11 +0000

    i added int main function

--- assignment_02_student_grade_system.cpp
+++ assignment_02_student_grade_system.cpp
@@ -65,3 +65,19 @@ char getGrade(int score) {
         return 'F';
     }
 }
+int main() {
+    int score;
+
+    cout << "Enter student score (0-100): ";
+    cin >> score;
+
+    char grade = getGrade(score);
+
+    if (grade == '\0') {
+        cout << "Error: Score must be between 0 and 100." << endl;
+    } else {
+        cout << "Grade: " << grade << endl;
+    }
+
+    return 0;
+}
